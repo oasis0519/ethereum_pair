c3e38d3ae6372090fbd26507170e96aa4400e5f3
seniordev0519 <seniordev0519@outlook.com>
2019-03-09 17:52:44 +0100

    The trouble is Solidity doesn't play well with dynamic length multidimensional arrays ... trying to work around now. Probably will redesign architecture to showcase the core functionality without relying on delegate calls to another contract to get data

--- contracts/LocationAware.sol
+++ contracts/LocationAware.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.2;
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
-contract PhysicalJurisdiction {
+contract Jurisdiction {
 
     address owner; // likely a jurisdiction's DAO contract address ...
     int[2][] public boundaries;
@@ -16,6 +16,7 @@ contract PhysicalJurisdiction {
     @param _boundaries: An array of ints representing coordinates of boundary vertices in nanodegrees (decimal degrees * 10**9)
 
     */
+
     constructor (int[] memory _boundaries, uint _tax, string memory _name) public {
 
         owner = msg.sender;
@@ -25,36 +26,36 @@ contract PhysicalJurisdiction {
         // Check to make sure _boundaries array is the right length
         require(l % 2 == 0, "Please pass in an even-length boundaries array of [lon1, lat1, lon2, lat2, ... , lonN, latN");
         // test that first point == last point
-        numBoundaryPoints = l / 2;
+        numBoundaryPoints = l;
 
         // Convert _boundaries array into int[2][] array of point pairs:
         // In same loop calculate bounding box
 
-        int minLon = 180 * 10**9; // should we use nanodegrees? 😬🤔
-        int minLat = 90 * 10**9;
-        int maxLon = -180 * 10**9;
-        int maxLat = -90 * 10**9;
+        // int minLon = 180 * 10**9; // should we use nanodegrees? 😬🤔
+        // int minLat = 90 * 10**9;
+        // int maxLon = -180 * 10**9;
+        // int maxLat = -90 * 10**9;
 
-        for (uint i = 0; i < l; i += 2) {
-            int[2] memory coords = [_boundaries[i], _boundaries[i + 1]];
+        // for (uint i = 0; i < l; i += 2) {
+        //     int[2] memory coords = [_boundaries[i], _boundaries[i + 1]];
 
-            if (coords[0] < minLon) {
-                minLon = coords[0];
-            }
-            if (coords[1] < minLat) {
-                minLat = coords[1];
-            }
-            if (coords[0] > maxLon) {
-                maxLon = coords[0];
-            }
-            if (coords[1] > maxLon) {
-                maxLat = coords[1];
-            }
+        //     if (coords[0] < minLon) {
+        //         minLon = coords[0];
+        //     }
+        //     if (coords[1] < minLat) {
+        //         minLat = coords[1];
+        //     }
+        //     if (coords[0] > maxLon) {
+        //         maxLon = coords[0];
+        //     }
+        //     if (coords[1] > maxLon) {
+        //         maxLat = coords[1];
+        //     }
 
-            boundaries.push(coords);
-        }
+        //     boundaries.push(coords);
+        // }
 
-        boundingBox = [[minLon, minLat], [maxLon, maxLat]];
+        // boundingBox = [[minLon, minLat], [maxLon, maxLat]];
         tax = _tax;
         name = _name;
 
@@ -116,7 +117,12 @@ contract LocationAwareWallet {
             address jurisdictionAddress = jurisdictions[i];
             // int[2][] memory jurisdictionPolygon = fetchJurisdictionPolygon(jurisdictionAddress);
 
-            if (withinJurisdictionBoundaries(_locationOfOwner, jurisdictionAddress) == true) {
+            Jurisdiction jurisdiction = Jurisdiction(jurisdictionAddress);
+
+            uint jurisdictionBoundariesLength = jurisdiction.numBoundaryPoints();
+            int[] memory jurisdictionBoundaries = new int[](jurisdictionBoundariesLength);
+
+            if (withinJurisdictionBoundaries(_locationOfOwner, jurisdictionBoundaries) == true) {
                 // transmit value to sender
                 address(_to).transfer(_value);
 
@@ -154,10 +160,10 @@ contract LocationAwareWallet {
 
     }
 
-    function withinJurisdictionBoundaries (int[2] memory _point, address _jurisdictionAddress) public returns (bool) {
+    function withinJurisdictionBoundaries (int[2] memory _point, int[] memory _jurisdictionBoundaries) public returns (bool) {
 
-        int[2][] memory jurisdictionBoundaries = fetchJurisdictionBoundaries(_jurisdictionAddress);
-        return pointInPolygon(_point, jurisdictionBoundaries);
+        // int[2][] memory jurisdictionBoundaries = fetchJurisdictionBoundaries(_jurisdictionAddress);
+        return pointInPolygon(_point, _jurisdictionBoundaries);
     }
 
     function getJurisdictionName (address _jurisdictionAddress) public returns (string memory jurisdictionName_) {
@@ -169,7 +175,7 @@ contract LocationAwareWallet {
     // Really helpful explanation by Tom MacWright on Observable,
     // https://observablehq.com/@tmcw/understanding-point-in-polygon
     // Caution: Would be easy to run out of gas by sending complex geometries.
-    function pointInPolygon (int[2] memory _point, int[2][] memory _polygon ) public returns (bool pointInsidePolygon_) {
+    function pointInPolygon (int[2] memory _point, int[] memory _polygon ) public returns (bool pointInsidePolygon_) {
 
         int x = _point[0];
         int y = _point[1];
@@ -178,12 +184,12 @@ contract LocationAwareWallet {
         uint l = _polygon.length;
 
         bool inside = false;
-        for (uint i = 0; i < l; j = i++) {
+        for (uint i = 0; i < l; j = i + 2) {
 
-            int xi = _polygon[i][0];
-            int yi = _polygon[i][1];
-            int xj = _polygon[j][0];
-            int yj = _polygon[j][1];
+            int xi = _polygon[i];
+            int yi = _polygon[i + 1];
+            int xj = _polygon[j];
+            int yj = _polygon[j + 1];
 
             bool intersect = ((yi > y) != (yj > y)) &&
                 (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
